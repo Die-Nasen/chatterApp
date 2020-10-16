afe35bbf73565a99ca134f94fd2d6117dc09f74b
SvLowman <SvenLohmann@gmx.de>
2020-10-16 11:02:36 +0200

    Add files

---
+++ src/components/anrufliste/anrufliste.stories.js
@@ -0,0 +1,7 @@
+import "./anrufliste.css";
+import entry from "./entry.html";
+export default {
+  title: "components/entry",
+  parameters: { layout: "centered" },
+};
+export const entryHTML = () => entry;
